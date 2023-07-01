db74607db1bf2590d8a0c40da527c6e734a4a1b8
eduardo pimenta <ed83isme@gmail.com>
2023-07-01 06:23:56 -0300

    added the User and Post models. Included the routes, controllers and view to stablish the minimal functionalities.

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,28 @@
+class PostsController < ApplicationController
+  before_action :authenticate_user!, except: [:index, :show]
+
+  def index
+    @posts = Post.all
+  end
+
+  def create
+    @post = Post.new(params[:post].permit(:title, :body))
+    @post.user = current_user
+    if @post.save
+      redirect_to @post
+    else
+      render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def new
+    @post = Post.new
+    @post.user = current_user
+  end
+
+  def show
+    @post = Post.find(params[:id])
+    @postAuthor = "Anonymous"
+    @postAuthor = @post.user.username if signed_in?
+  end
+end
